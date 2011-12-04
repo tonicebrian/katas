884c49dc69d1cb32577de60318cbcbdf1c288366
Toni Cebrián <ancechu@gmail.com>
2011-12-04 23:48:21 +0100

    Refactoring so tests are clearer

--- KataPotter/src/KattaPotter.hs
+++ KataPotter/src/KattaPotter.hs
@@ -3,3 +3,9 @@ module KattaPotter where
 price :: [Int] -> Double
 price [] = 0
 price _ = -1
+
+priceDiscount 1 = 8
+priceDiscount 2 = 8 * 2 * 0.95
+priceDiscount 3 = 8 * 3 * 0.9
+priceDiscount 4 = 8 * 4 * 0.8
+priceDiscount 5 = 8 * 5 * 0.75
