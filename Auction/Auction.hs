5adc02d2948298bcd5a02f27a00dc54cdc2e1a8a
Toni Cebrián <ancechu@gmail.com>
2012-03-05 17:51:25 +0100

    First version of the Auction kata

--- Auction/Auction.hs
+++ Auction/Auction.hs
@@ -1,4 +1,6 @@
 
+import System.Environment
+
 data Product = P Double Double -- Price & Weight
 
 instance Eq Product where
@@ -7,7 +9,16 @@ instance Eq Product where
 instance Ord Product where
   compare (P p1 w1) (P p2 w2) 
      | ((p1 < p2) && (w1 <= w2)) || ((p1 <= p2) && (w1 < w2)) = GT 
-     | otherwise  = LT                             
+     | otherwise  = LT
 
 isBargain :: Product -> [Product] -> Bool
 isBargain p ps = p == (maximum ps)
+  
+isTerrible :: Product -> [Product] -> Bool
+isTerrible p ps = p == (minimum ps)  
+
+main = do
+  args <- getArgs
+  contents <- readFile (args !! 0)
+  let problems = map (map (read :: String->Int) . words) $ tail $ lines contents
+  putStrLn "Hello world!!"
