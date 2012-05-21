dba792a0ee685d731cdc343db99f8213894da381
Toni Cebrián <ancechu@gmail.com>
2012-05-21 22:21:18 +0200

    Final version

--- LambdaTheGathering/src/haskell/Client.hs
+++ LambdaTheGathering/src/haskell/Client.hs
@@ -1,6 +1,6 @@
 import GameState_Client
 import Service_Types
-import Network
+import Network 
 import System.IO
 import System.Environment (getArgs)
 
@@ -15,21 +15,36 @@ import Thrift.Server
 port = 4390
 host = "127.0.0.1"
 
-testMove = Move (Just 0) (Just 1) (Just 2) (Just "dbl")
+readOrders :: FilePath -> IO [Move]
+readOrders filename = do
+    content <- readFile filename
+    let ws = map words $ lines content
+    let mkMove desc = Move (Just a) (Just b) (Just c) (Just d)
+            where
+                a = nums !! 0
+                b = nums !! 1
+                c = nums !! 2
+                d = desc !! 3
+                nums = map (\x -> read x) $ take 3 desc
+    return $ map mkMove ws
 
-clientFunc :: HostName -> PortNumber -> IO ()
-clientFunc host p = do
+clientFunc :: HostName -> PortNumber -> FilePath ->  IO ()
+clientFunc host p fileOrders = do
   putStrLn "Client go!"
   h <- connectTo host $ PortNumber p
   let proto = BinaryProtocol h
-  processMove (proto,proto) testMove
-  tFlush h
-  putStrLn "Move sent"
+  orders <- readOrders fileOrders
+  mapM_ (\m -> processMove (proto,proto) m >> tFlush h) orders
+  putStrLn "Moves sent"
   slots <- getLastUpdate (proto,proto)
   putStrLn "Receiving slots"
   print slots
   tClose h
 
 main = do
-    clientFunc host port
+    args <- getArgs
+    let (host',port',ordersFile) = if (length args == 0 )
+                            then (host,port,"test/resources/Orders.txt")
+                            else (args!!0, fromIntegral $ (read (args!!1) :: Integer),args !! 2)
+    clientFunc host port ordersFile
     
--- LambdaTheGathering/src/haskell/Server.hs
+++ LambdaTheGathering/src/haskell/Server.hs
@@ -19,17 +19,17 @@ host = "127.0.0.1"
 data ServerCore = ServerCore
 
 instance GameState_Iface ServerCore where
-    processMove a (Just m) = putStrLn "Recibido un Move"
+    processMove a (Just m) = print m
     processMove a Nothing = putStrLn "Recibido un Nothing"
     getLastUpdate a = do
              putStrLn "Recibido un Last update"
              return $ [Slot (Just 0) (Just 1) (Just "zero")]
 
-serverFunc :: a -> (BinaryProtocol Handle, BinaryProtocol Handle) -> IO Bool
-serverFunc = undefined
-
 main = do
     args <- getArgs
-    runBasicServer ServerCore GameState.process port
+    let port' = if (length args == 0 )
+                then port
+                else fromIntegral $ (read (args!!0) :: Integer)
+    runBasicServer ServerCore GameState.process port'
     putStrLn "Stopping the server"
     
